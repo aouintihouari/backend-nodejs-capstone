fdfdbcae5f3905b38f9ef6df71cf244cea5c9ebb
Houari Aouinti <169095690+aouintihouari@users.noreply.github.com>
2025-11-26 06:28:53 +0100

    Refactor secondChanceItemsRoutes for consistency

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -1,95 +1,127 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-const router = express.Router();
-const connectToDatabase = require('../models/db');
-const logger = require('../logger');
+const express = require('express')
+const multer = require('multer')
+const router = express.Router()
+const connectToDatabase = require('../models/db')
+const logger = require('../logger')
 
-// Define the upload directory path
-const directoryPath = 'public/images';
+const directoryPath = 'public/images'
 
-// Set up storage for uploaded files
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, directoryPath); // Specify the upload directory
+    cb(null, directoryPath) 
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname); // Use the original file name
-  },
-});
+    cb(null, file.originalname)
+  }
+})
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage })
 
+const collectionName = 'secondChanceItems'
 
-// Get all secondChanceItems
 router.get('/', async (req, res, next) => {
-    logger.info('/ called');
-    try {
-        //Step 2: task 1 - insert code here
-        //Step 2: task 2 - insert code here
-        //Step 2: task 3 - insert code here
-        //Step 2: task 4 - insert code here
+  logger.info('/ called')
+  try {
+    const db = await connectToDatabase()
 
-        const collection = db.collection("secondChanceItems");
-        const secondChanceItems = await collection.find({}).toArray();
-        res.json(secondChanceItems);
-    } catch (e) {
-        logger.console.error('oops something went wrong', e)
-        next(e);
-    }
-});
+    const collection = db.collection(collectionName)
 
-// Add a new item
-router.post('/', {Step 3: Task 6 insert code here}, async(req, res,next) => {
-    try {
+    const secondChanceItems = await collection.find({}).toArray()
 
-        //Step 3: task 1 - insert code here
-        //Step 3: task 2 - insert code here
-        //Step 3: task 3 - insert code here
-        //Step 3: task 4 - insert code here
-        //Step 3: task 5 - insert code here
-        res.status(201).json(secondChanceItem.ops[0]);
-    } catch (e) {
-        next(e);
-    }
-});
+    res.json(secondChanceItems)
+  } catch (e) {
+    logger.console.error('oops something went wrong', e)
+    next(e)
+  }
+})
 
-// Get a single secondChanceItem by ID
+router.post('/', upload.single('file'), async (req, res, next) => {
+  try {
+    const db = await connectToDatabase()
+
+    const collection = await db.collection(collectionName)
+    let newItem = req.body
+
+    const lastItem = collection.find().sort({ id: -1 }).limit(1)
+    await lastItem.forEach(item => {
+      newItem.id = (parseInt(item.id) + 1).toString()
+    })
+    
+    const dateAdded = Math.floor(new Date().getTime() / 1000)
+    newItem.date_added = dateAdded
+
+    newItem = await collection.insertOne(newItem)
+
+    res.status(201).json(newItem)
+  } catch (e) {
+    next(e)
+  }
+})
+
 router.get('/:id', async (req, res, next) => {
-    try {
-        //Step 4: task 1 - insert code here
-        //Step 4: task 2 - insert code here
-        //Step 4: task 3 - insert code here
-        //Step 4: task 4 - insert code here
-    } catch (e) {
-        next(e);
-    }
-});
+  try {
+    const id = req.params.id
+    const db = await connectToDatabase()
 
-// Update and existing item
-router.put('/:id', async(req, res,next) => {
-    try {
-        //Step 5: task 1 - insert code here
-        //Step 5: task 2 - insert code here
-        //Step 5: task 3 - insert code here
-        //Step 5: task 4 - insert code here
-        //Step 5: task 5 - insert code here
-    } catch (e) {
-        next(e);
-    }
-});
+    const collection = await db.collection(collectionName)
+
+    const secondChanceItem = await collection.findOne({ id: id })
+
+    if (!secondChanceItem) return res.status(404).json('Item not found.')
+    res.json(secondChanceItem)
+  } catch (e) {
+    next(e)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const db = await connectToDatabase()
+
+    const collection = await db.collection(collectionName)
+    const item = await collection.findOne({ id: id })
+
+    if (!item) 
+      return res.status(404).json('Item not found.')
+    
+    item.category = req.body.category
+    item.condition = req.body.condition
+    item.age_days = req.body.age_days
+    item.description = req.body.description
+    item.age_years = Number((req.body.age_days / 365).toFixed(1))
+    item.updatedAt = new Date()
+
+    const updatedItem = await collection.findOneAndUpdate(
+      { id: id },
+      { $set: item },
+      { returnDocument: 'after' }
+    )
+    if (updatedItem) 
+      res.json({ uploaded: 'success' })
+    else 
+      res.json({ uploaded: 'failed' })
+  } catch (e) {
+    next(e)
+  }
+})
 
 // Delete an existing item
-router.delete('/:id', async(req, res,next) => {
-    try {
-        //Step 6: task 1 - insert code here
-        //Step 6: task 2 - insert code here
-        //Step 6: task 3 - insert code here
-        //Step 6: task 4 - insert code here
-    } catch (e) {
-        next(e);
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const db = await connectToDatabase()
+    const collection = await db.collection(collectionName)
+    const item = await collection.findOne({ id: id })
+    if (!item) {
+      logger.error('secondChanceItem not found')
+      return res.status(404).json({ error: 'secondChanceItem not found' })
     }
-});
+    await collection.deleteOne(item)
+    res.json({ deleted: 'success' })
+  } catch (e) {
+    next(e)
+  }
+})
 
-module.exports = router;
+module.exports = router
